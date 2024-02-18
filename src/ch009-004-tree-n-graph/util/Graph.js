a7b73834e0371febaaa34bfef3a52b9144301764
youngbeomrhee <youngbeomrhee@gmail.com>
2024-02-18 22:46:45 +0800

    feat: 개별 node와 함수에서 분산되어 처리되던 로직을 Graph class로 응집

--- src/ch009-004-tree-n-graph/util/Graph.js
+++ src/ch009-004-tree-n-graph/util/Graph.js
@@ -10,7 +10,55 @@ export class GraphNode {
 
 export class Graph {
     constructor() {
-        this.nodes = null
+        this.nodes = {}
+    }
+    addNode(name) {
+        this.nodes[name] = new GraphNode(name)
+    }
+    addEdge(from, to) {
+        if (this.nodes[from] && this.nodes[to]) {
+            this.nodes[from].adjacent.push(this.nodes[to])
+        }
+    }
+    clearVisitHistory() {
+        // 기존 방문 히스토리 초기화
+        Object.values(this.nodes).forEach((node) => (node.visited = false))
+    }
+    depthFirstSearch(visit = console.log) {
+        this.clearVisitHistory()
+        const dfs = (node, visit) => {
+            if (!node) {
+                return
+            }
+            visit(node)
+            node.visited = true
+
+            node.adjacent.forEach((node) => {
+                if (node.visited === false) {
+                    dfs(node, visit)
+                }
+            })
+        }
+        dfs(Object.values(this.nodes)[0], visit)
+    }
+    breadthFirstSearch(visit = console.log) {
+        this.clearVisitHistory()
+        const root = Object.values(this.nodes)[0]
+        if (root) {
+            const queue = new LinkedListQueue()
+            root.visited = true
+            queue.add(root)
+            while (!queue.isEmpty()) {
+                const r = queue.remove()
+                visit(r)
+                r.adjacent.forEach((node) => {
+                    if (node.visited === false) {
+                        node.visited = true
+                        queue.add(node)
+                    }
+                })
+            }
+        }
     }
 }
 
--- test/ch009-004-tree-n-graph/util/Graph.test.js
+++ test/ch009-004-tree-n-graph/util/Graph.test.js
@@ -1,4 +1,5 @@
 import {
+    Graph,
     GraphNode,
     breadthFirstSearch,
     depthFirstSearch,
@@ -6,28 +7,32 @@ import {
 import { Logger } from '../../../src/ch009-004-tree-n-graph/util/Logger'
 
 describe('Graph test', () => {
-    let root
+    let graph
     beforeEach(() => {
-        const node0 = new GraphNode(0)
-        const node1 = new GraphNode(1)
-        const node2 = new GraphNode(2)
-        const node3 = new GraphNode(3)
-        const node4 = new GraphNode(4)
-        const node5 = new GraphNode(5)
-        node0.adjacent = [node1, node4, node5]
-        node1.adjacent = [node3, node4]
-        node2.adjacent = [node1]
-        node3.adjacent = [node2, node4]
-        root = node0
+        graph = new Graph()
+        graph.addNode(0)
+        graph.addNode(1)
+        graph.addNode(2)
+        graph.addNode(3)
+        graph.addNode(4)
+        graph.addNode(5)
+        graph.addEdge(0, 1)
+        graph.addEdge(0, 4)
+        graph.addEdge(0, 5)
+        graph.addEdge(1, 3)
+        graph.addEdge(1, 4)
+        graph.addEdge(2, 1)
+        graph.addEdge(3, 2)
+        graph.addEdge(3, 4)
     })
     test('depthFirstSearch', () => {
         const logger = new Logger()
-        depthFirstSearch(root, (node) => logger.push(node.name))
+        graph.depthFirstSearch((node) => logger.push(node.name))
         expect(logger.log).toStrictEqual([0, 1, 3, 2, 4, 5])
     })
     test('breadthFirstSearch', () => {
         const logger = new Logger()
-        breadthFirstSearch(root, (node) => logger.push(node.name))
+        graph.breadthFirstSearch((node) => logger.push(node.name))
         expect(logger.log).toStrictEqual([0, 1, 4, 5, 3, 2])
     })
 })
